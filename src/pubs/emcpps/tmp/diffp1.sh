4617d5fdfc9a7c208fc88e578622dff76bf20859
Joshua Berne <jberne4@bloomberg.net>
2021-01-25 20:47:03 +0000

    initial listings for packet 1 done

--- src/pubs/emcpps/tmp/diffp1.sh
+++ src/pubs/emcpps/tmp/diffp1.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
+ODIR=../tmp-p1_listings
+
+rm -f "${ODIR}/*"
+mkdir -p "${ODIR}"
+
 for x in binaryliteral consecutiveangle decltype delete override staticassert trailingreturn unrestrictedunion noreturn nullptr ; do
-  echo "----------------------------${x}"
-  diff -w  ../tmp-2021_01_18/${x}.tex emcppsf_${x}.tex 
+    echo "----------------------------${x}"
+
+    cp emcppsf_${x}.tex ${ODIR}/${x}.tex
+    
+  diff -w  ../tmp-2021_01_18/${x}.tex ${ODIR}/${x}.tex 
   echo
 done
