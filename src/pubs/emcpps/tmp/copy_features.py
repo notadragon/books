3ee0c5439f69ce8f62173a19edf19e397269016e
notadragon <berne@notadragon.com>
2021-01-30 15:20:53 -0500

    new baseline from lori

--- src/pubs/emcpps/tmp/copy_features.py
+++ src/pubs/emcpps/tmp/copy_features.py
@@ -11,6 +11,8 @@ masterfiles={ "Ch1.tex": "safe",
 lversions=[ "cpp11", "cpp14" ]
 
 
+outlineSectionRe = re.compile("\\[(.*)\\]")
+outlineReplaceSectionRe = re.compile("(safe|conditionallysafe|unsafe)_(cpp11|cpp14)")
 outline = []
 
 inputRe = re.compile("^\\\\input\{(.*)\}$")
@@ -31,6 +33,31 @@ def updatefile(tof, outlines):
         print(f"  Creating: {tof}")
         tof.open("w").writelines(outlines)
 
+labelRe = re.compile("\\\\label\{(.*?)\}")
+secRe = re.compile("\\\\section\[(.*?)\]\{(.*?)\}\\\\label\{(.*?)\}")
+def processHead(lines):
+    output = []
+    shortTit = None
+    longTit = None
+    labels = []
+    for l in lines:
+        for lab in labelRe.findall(l):
+            labels.append(lab)
+        for sec in secRe.findall(l):
+            shortTit = sec[0]
+            longTit = sec[1]
+
+    #print(f"SHORT: {shortTit}  LONG: {longTit}  LABELS: {labels}")
+
+    output.append(f"\emcppsFeature{{")
+    output.append(f"        short={{{shortTit}}},")
+    output.append(f"        long={{{longTit}}},")
+    output.append(f"}}{{{labels[0]}}}")
+
+    for l in labels[1:]:
+        output.append(f"\\label{{{l}}}")
+    
+    return output
 
 for fname, sectionroot in masterfiles.items():
     vindex = 0
@@ -56,10 +83,18 @@ for fname, sectionroot in masterfiles.items():
 
         sec = sectionroot + "_" + lversions[vindex]
         inp = m.group(1)
-        
+
+        out = inp
+        if out.endswith("-josh"):
+            out = out[:-5]
+        if out.endswith("-josh2"):
+            out = out[:-6]
+
+        outlines = processHead(outlines)
+            
         fromf = pathlib.Path(f"{inp}.tex")
-        tof = pathlib.Path(f"../emcppsf/emcppsf_{inp}.tex")
-        tof2 = pathlib.Path(f"../emcppsf/emcppsf_{inp}.tex.bak")
+        tof = pathlib.Path(f"../emcppsf/emcppsf_{out}.tex")
+        tof2 = pathlib.Path(f"../emcppsf/emcppsf_{out}.tex.bak")
 
         outlines.extend(open(fromf).readlines())
 
@@ -69,13 +104,32 @@ for fname, sectionroot in masterfiles.items():
             if outline:
                 outline.append("")
             outline.append(f"[{sec}]")
-        outline.append(f"emcppsf_{inp}")
+        outline.append(f"emcppsf_{out}")
 
 
         updatefile(tof,outlines)
         outlines = []
 
-outline = [ l.rstrip() + "\n" for l in outline ]
+        
 outlinefile = pathlib.Path(f"../emcppsf/emcppsf.outline")
 
+keep = False
+for line in outlinefile.open().readlines():
+    m = outlineSectionRe.match(line.rstrip())
+    if m:
+        outlinegroup = m.group(1)
+        m2 = outlineReplaceSectionRe.match(outlinegroup)
+        if m2:
+            print(f"Dropping section {outlinegroup}")
+            keep = False
+        else:
+            if not keep:
+                outline.append("")
+            keep = True
+
+    if keep:
+        outline.append(line)
+
+
+outline = [ l.rstrip() + "\n" for l in outline ]
 updatefile(outlinefile, outline)
